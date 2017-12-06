b756eec89786668d3bffd632678741bd0b34afb8
hexoishexo <xxpang@TrumpTower.local>
2017-12-06 17:09:03 +0800

    修复测试错误
    Python 对缩进真的很严格，把 tab 都换成空格测试就通过了

--- polls/tests.py
+++ polls/tests.py
@@ -15,39 +15,20 @@ class QuestionModelTests(TestCase):
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
-	    """
-	    was_published_recently() returns False for questions whose pub_date
-	    is older than 1 day.
-	    """
-	    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-	    old_question = Question(pub_date=time)
-	    self.assertIs(old_question.was_published_recently(), False)
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
 
-	def test_was_published_recently_with_recent_question(self):
-	    """
-	    was_published_recently() returns True for questions whose pub_date
-	    is within the last day.
-	    """
-	    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-	    recent_question = Question(pub_date=time)
-	    self.assertIs(recent_question.was_published_recently(), True)
-"""
-python3 manage.py test polls
-
-Creating test database for alias 'default'...
-System check identified no issues (0 silenced).
-F
-======================================================================
-FAIL: test_was_published_recently_with_future_question (polls.tests.QuestionModelTests)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "/Users/xxpang/Documents/WorkSpace/GithubDesktop/DjangoSample/polls/tests.py", line 15, in test_was_published_recently_with_future_question
-    self.assertIs(future_question.was_published_recently(), False)
-AssertionError: True is not False
-
-----------------------------------------------------------------------
-Ran 1 test in 0.001s
-
-FAILED (failures=1)
-Destroying test database for alias 'default'...
-"""
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+# python3 manage.py test polls
