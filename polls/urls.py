e1497889d0942626799aa39fb0113d75196c4091
hexoishexo <xxpang@TrumpTower.local>
2017-12-06 15:58:34 +0800

    till app_name

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,19 @@
+# -*- coding: UTF-8 -*-
+'''
+There’s no need to add URL cruft such as .html – unless you want to, in which case you can do something like this:
+
+	path('polls/latest.html', views.index),
+
+But, don’t do that. It’s silly.
+'''
 from django.urls import path
 
 from . import views
+
+# Namespacing 用来区分不同的 app
+# 因为当前工程中只有 polls 一个 app， 如果以后多了区分 url 等便会产生麻烦
+# 加了命名空间之后，在 html 中的 url 前面加上相应的 app_name 即可
+app_name = 'polls'
 
 urlpatterns = [
     # ex: /polls/
