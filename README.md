c972940372684e30b092d4f05a2144e2bc6ca2c0
hexoishexo <xxpang@TrumpTower.local>
2017-12-06 18:19:06 +0800

    add view test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Take a deep dive in Django
 [Offical Django tutorial](https://docs.djangoproject.com/en/2.0/intro/)
 
-tutorial 1-4/5
+tutorial 1-5
--- polls/views.py
+++ polls/views.py
@@ -6,22 +6,27 @@ from django.template import loader
 
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-
-from django.urls import reverse
 from django.views import generic
+from django.urls import reverse
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
